da8084c82ce94356b16d8d61d1659407cba3fbe5
~Haynesss~ <zerossb@gmail.com>
2021-09-21 17:02:49 -0300

    Update entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -11,10 +11,15 @@ npm -v
 echo "Check python Version"
 python -V
 
-echo "Installing Serverless and plugins..."
-npm i serverless -g
-npm i serverless-python-requirements
-npm i serverless-plugin-canary-deployments
+echo "Check package.json"
+if [[ ! -f "$GITHUB_WORKSPACE/package-lock.json" ]]; then
+  npm install
+else
+  echo "Installing Serverless and plugins..."
+  npm i serverless -g
+  npm i serverless-python-requirements
+  npm i serverless-plugin-canary-deployments
+fi
 
 if [ "$ENVIRONMENT" ]; then
   echo "Running sls deploy $ARGS..."
@@ -26,7 +31,9 @@ if [ "$ENVIRONMENT" ]; then
 else
   echo "Running sls deploy $ARGS..."
   if [ "$AWS_ACCESS_KEY_ID" ] && [ "$AWS_SECRET_ACCESS_KEY" ]; then
+    echo "sls config credentials $ARGS..."
     sls config credentials --provider aws --key "$AWS_ACCESS_KEY_ID" --secret "$AWS_SECRET_ACCESS_KEY"
   fi
-  sls deploy "$ARGS"
+  echo "Running sls deploy $ARGS..."
+  sls deploy $ARGS
 fi
