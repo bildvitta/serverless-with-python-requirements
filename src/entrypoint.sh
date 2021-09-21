eebe6051c26ed967dd1787ff5fd17885664ab5f3
Guilherme Haynes Howe <zerossb@gmail.com>
2021-09-21 14:15:30 -0300

    First Actions

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -16,8 +16,15 @@ npm i serverless -g
 npm i serverless-python-requirements
 npm i serverless-plugin-canary-deployments
 
-echo "Running sls deploy $ARGS..."
-if [ "$AWS_ACCESS_KEY_ID" ] && [ "$AWS_SECRET_ACCESS_KEY" ]; then
-  sls config credentials --provider aws --key "$AWS_ACCESS_KEY_ID" --secret "$AWS_SECRET_ACCESS_KEY" $ARGS
+if [ "$ENVIRONMENT" ]; then
+  echo "Running sls deploy $ARGS..."
+  echo "Running sls deploy $AWS_SECRET_ACCESS_KEY..."
+  echo "Running sls deploy $AWS_ACCESS_KEY_ID..."
+  echo "Running sls deploy $ENVIRONMENT..."
+else
+  echo "Running sls deploy $ARGS..."
+  if [ "$AWS_ACCESS_KEY_ID" ] && [ "$AWS_SECRET_ACCESS_KEY" ]; then
+    sls config credentials --provider aws --key "$AWS_ACCESS_KEY_ID" --secret "$AWS_SECRET_ACCESS_KEY" $ARGS
+  fi
+  sls deploy "$ARGS"
 fi
-sls deploy "$ARGS"
